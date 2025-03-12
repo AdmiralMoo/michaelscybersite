19b4d7a4f2648307f4b8b349064cf6d77f51ef85
AngryTurboprop <michael_vogl@outlook.com>
2025-03-11 23:31:22 -0400

    03/11/25 - Revision 3
    -Made major progress on the music page rewrite
    -Head code is now automatically injected into each page!
    -Added some more badges to the badges folder for later use
    -Rewrote parts of the albums of the month display. To consider the music page completed, I need to figure out how to present it on mobile sized screens
    -Removed base.js
    -Moved music.css to CSS folder

--- head-inject.js
+++ head-inject.js
@@ -1,14 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const headContent = `
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!--<!--<!--<base href="https://michaelscybersite.net/">-->-->-->
-        <base href="http://127.0.0.1:5500/">
-        <link rel="icon" type="image/x-icon" href="/assets/site/favicon.ico">
-        <link rel="stylesheet" type="text/css" href="index.css"/>
-        <link rel="preload" href="/assets/ui/button_down.jpg" as="image">
-        <link rel="preload" href="/assets/ui/button_hover.jpg" as="image">
-    `;
+function injectHead() {
+    document.head.innerHTML = 
+    `<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<base href="https://michaelscybersite.net/">-->
+    <base href="http://127.0.0.1:5500/">
+    <link rel="stylesheet" type="text/css" href="/assets/css/base.css"/>
+    <link rel="icon" type="image/x-icon" href="/assets/site/favicon.ico">
+    ` 
+    + document.head.innerHTML;
+}
 
-    document.head.innerHTML += headContent;
-});
+window.onloadstart = injectHead();
