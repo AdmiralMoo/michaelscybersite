02a30f501cb066443924f00f526690a2d6e28f85
Michael Vogl <122392823+AngryTurboprop@users.noreply.github.com>
2025-03-14 12:46:44 -0400

    Added an album showcase widget to the music screen Made all albums and bands on the music page clickable links

---
+++ scripts/album-showcase.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+    updateDisplay(); // Initialize display when page loads
+});
+
+let currentAlbumIndex = 0;
+let albumsDatabase = [];
+
+function updateDisplay() {
+    fetch('./assets/json/albumShowcaseData.json')
+    .then(response => response.json())
+    .then(data => {
+        albumsDatabase = data; // Assign fetched data to albumsDatabase
+        console.log(albumsDatabase);
+
+        const albumShowcaseParent = document.getElementById("albumShowcaseBody");
+        const album = albumsDatabase[currentAlbumIndex];
+
+        albumShowcaseParent.innerHTML = "";
+
+        let tracksList = "";
+        let trackIndex = 0;
+        album.tracks.forEach(track => {
+            trackIndex += 1;
+            tracksList += `<li>${track}</li>`;
+        });
+
+        albumShowcaseParent.innerHTML += `
+            <div class="newstitle side-by-side" style="padding-top:20px;">
+                <p id="showcaseAlbumArtistName">${album.artist}/${album.album}</p>
+                <p id="showcaseAlbumYear">${album.year}</p>
+            </div>
+            <div class="side-by-side">
+                <p id="showcaseAlbumTrackCount">${album.tracks.length} Tracks</p>
+                <p id="showcaseAlbumDuration">Duration: ${album.duration}</p>
+            </div>
+            
+            <div class="albumShowcaseContent outsideborder" style="display: flex; flex-direction: column;">
+                <div class="side-by-side albumShowcaseTopDisplay">
+                    <img id="showcaseAlbumCover" src="${album.cover}">
+                    <div class="box-container">
+                        <h4>Tracks</h4>
+                        <ol id="showcaseAlbumTracklist">`
+                        + tracksList + `
+                        </ol>
+                    </div>
+                </div>
+                <div class="box-container" >
+                    <p id="showcaseAlbumDescription">${album.description}</p>
+                </div>
+            </div>
+        `;
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+}
+
+function shiftAlbum() {
+    let newIndex = -1;
+    while (true) {
+        newIndex = Math.floor(Math.random() * albumsDatabase.length);
+        if (newIndex !== currentAlbumIndex) {
+            currentAlbumIndex = newIndex;
+            break;
+        }
+    }
+    updateDisplay();
+}
+
+
+document.getElementById("shuffleAlbum").addEventListener("click", () => shiftAlbum());
+
+// Initialize the display
+updateDisplay();
