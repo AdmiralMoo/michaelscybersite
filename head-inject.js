c8475dcb49868dd389cc1e6cafccaf0e9eb1f867
AngryTurboprop <michael_vogl@outlook.com>
2025-03-14 01:19:40 -0400

    30/13/25 - Revision 1
    -Began working on a Skin Yard fan page
    -Added colour to the title bars of header elements
    Disabled text selection on button labels
    -Updated the statter page so that it works on mobile and scales dynamically
    -Added Emma by Jane Austen to the books page
    -Made adjustments to CSS to support iOS properly

--- head-inject.js
+++ head-inject.js
@@ -2,8 +2,9 @@ function injectHead() {
     document.head.innerHTML = 
     `<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="https://michaelscybersite.net/">
+    <base href="https://michaelscybersite.net/">>
     <!--<base href="http://127.0.0.1:5500/">-->
+    <!--<base href="http://10.0.0.215:5500/">-->
     <link rel="stylesheet" type="text/css" href="/assets/css/base.css"/>
     <link rel="icon" type="image/x-icon" href="/assets/site/favicon.ico">
     ` 
