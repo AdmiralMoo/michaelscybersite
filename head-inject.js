b078816881881fd6eedf2ec3f33fe1e28db115f1
AngryTurboprop <michael_vogl@outlook.com>
2025-03-16 11:07:37 -0400

    Update head-inject.js

--- head-inject.js
+++ head-inject.js
@@ -3,9 +3,9 @@ function injectHead() {
     `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<base href="https://michaelscybersite.net/">-->
+    <base href="https://michaelscybersite.net/">
     <!--<base href="http://127.0.0.1:5500/">-->
-    <base href="http://10.0.0.215:5500/">
+    <!--<base href="http://10.0.0.215:5500/">-->
     <link rel="stylesheet" type="text/css" href="/assets/css/base.css"/>
     <link rel="icon" type="image/x-icon" href="/assets/site/favicon.ico">
     ` 
