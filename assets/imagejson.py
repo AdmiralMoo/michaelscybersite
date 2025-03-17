fa278e4090fe23c03e3f47443d05d9cf85940a6c
AngryTurboprop <michael_vogl@outlook.com>
2025-03-17 14:29:14 -0400

    03/07/25 - Revision 2
    -Added more newspaper headlines
    -Made incremental improvements to imagejson.py
    -Added Last Stand At Papago Wells
    -Added more albums to the album showcase
    -Spelling corrections on the album showcase

--- assets/imagejson.py
+++ assets/imagejson.py
@@ -6,7 +6,7 @@ def BuildFile(image_dir, output_file):
     valid_extensions = (".jpg", ".jpeg", ".png", ".gif", ".webp")
 
     # List all valid image files in the directory
-    images = [f for f in os.listdir(image_dir) if f.lower().endswith(valid_extensions)]
+    images = [f for f in os.listdir(os.getcwd()+image_dir) if f.lower().endswith(valid_extensions)]
 
     # Save the list to a JSON file
     with open(output_file, "w") as json_file:
@@ -18,16 +18,16 @@ print("A. Books\nB. CDs\nC. Games")
 image_dir = "none"
 uinput = input(">")
 if "a" in uinput.lower():
-    image_dir = "books"
+    image_dir = r"\assets\books"
 elif "b" in uinput.lower():
-    image_dir = "albumart"
+    image_dir = r"\assets\lbumart"
 elif "c" in uinput.lower():
-    image_dir = "games"
+    image_dir = r"\assets\games"
 else:
     print("bad input: " + uinput)
 
 print("alright, " + image_dir)
-output_file = "images.json"
+output_file = os.getcwd() + image_dir + r"\images.json"
 
 if image_dir != "none":
     BuildFile(image_dir, output_file)
