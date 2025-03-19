f1c4940ff24e110bae1e6618ca41526ef0a0b921
AngryTurboprop <michael_vogl@outlook.com>
2025-03-19 00:27:00 -0400

    03/18/25 - Revision 3
    -Added On Cats by Charles Bukowski
    -Scanned all my cassette tapes and made a page to display them
    -Put together a basic page to show my CDs
    -Added more newspaper headlines
    -Added more albums to the album showcase
    -Put together a page for the vertical album Phenomenon

--- assets/imagejson.py
+++ assets/imagejson.py
@@ -14,7 +14,7 @@ def BuildFile(image_dir, output_file):
 
     print(f"Generated {output_file} with {len(images)} images.")
 
-print("A. Books\nB. CDs\nC. Games")
+print("A. Books\nB. CDs\nC. Games\nD. Cassettes")
 image_dir = "none"
 uinput = input(">")
 if "a" in uinput.lower():
@@ -23,6 +23,8 @@ elif "b" in uinput.lower():
     image_dir = r"\assets\lbumart"
 elif "c" in uinput.lower():
     image_dir = r"\assets\games"
+elif "d" in uinput.lower(): 
+    image_dir = r"\assets\cassettes"
 else:
     print("bad input: " + uinput)
 
