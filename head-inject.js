752290963db387e5985e5974e503cdea7c1b3cdc
AngryTurboprop <michael_vogl@outlook.com>
2025-03-26 21:44:11 -0400

    03/26/25 - Revision 2
    -Base href now gets set automatically depending on if the site is deployed or running locally during development

--- head-inject.js
+++ head-inject.js
@@ -14,12 +14,22 @@ if (document.readyState === "loading") {
 
 //Inject Head data
 function injectHead(injectHeader) {
+    //Detect if the site is local or on GitHub Pages
+    let baseHref;
+    if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+        baseHref = "http://127.0.0.1:5500/";
+    } else if (window.location.hostname === "10.0.0.215"){
+        baseHref = "http://10.0.0.215:5500/";
+    } else if (window.location.hostname == "localhost:5500"){
+        baseHref = "http://localhost:5500/";
+    } else {
+        baseHref = "https://michaelscybersite.net/";
+    }
+
     document.head.insertAdjacentHTML("afterbegin", `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="https://michaelscybersite.net/">
-    <!--<base href="http://127.0.0.1:5500/">-->
-    <!--<base href="http://10.0.0.215:5500/">-->
+    <base href="${baseHref}">
     <link rel="stylesheet" type="text/css" href="/assets/css/base.css"/>
     <link rel="icon" type="image/x-icon" href="/assets/site/favicon.ico">
     `);
