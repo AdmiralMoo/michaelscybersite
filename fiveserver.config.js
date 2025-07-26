269a0f6e43e4554b5d22c6fd933148c91e4c60a6
AngryTurboprop <michael_vogl@outlook.com>
2025-07-26 18:57:03 -0400

    07/26/25 - Revision 1
    -Edited the spine of 2001: A Space Odyssey to get rid of the white spot where there should be a VHS tape
    -Worked on the VHS movies shelf
    -Fixed the bookshelf box's overflow and scaling
    -Added planks of wood to make it an actual shelf
    -Added automatically generated shadows to the VHS tapes
    -Added tags to all VHS tapes
    -Added more tapes to the shelf
    -Added more tape scans to the files
    -Added wood to the VHS shelf
    -Updated news

---
+++ fiveserver.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  highlight: false, // enable highlight feature
+  injectBody: false, // enable instant update
+  remoteLogs: false, // enable remoteLogs
+  remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+  injectCss: true, // disable injecting css
+  navigate: true, // enable auto-navigation+module.exports = {
+  host: '0.0.0.0', // default: '0.0.0.0' (could also be 'localhost' for example)
+  remoteLogs: 'magenta', // true | false | Color
+  useLocalIp: false, // optional: opens browser with your local IP
+}
